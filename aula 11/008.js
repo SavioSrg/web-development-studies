9d2bc2000b53502469c71228253778365d0fc900
SavioSrg <saviosrg@gmail.com>
2024-11-21 17:52:14 -0300

    atualizações

---
+++ aula 11/008.js
@@ -0,0 +1,8 @@
+var vel = 40.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MuLTADO!')
+} else{
+    console.log('Velocidade correta da via')
+}
+console.log(`Dirija sempre usando cinto de segurrança!`)
