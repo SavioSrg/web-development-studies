cafc64c366020b0d55030426952bcc5d78e207c4
SavioSrg <saviosrg@gmail.com>
2024-12-06 13:54:52 -0300

    correções no exercicio 003

--- exercicio_projeto/ex003/script.js
+++ exercicio_projeto/ex003/script.js
@@ -1,20 +1,18 @@
-
-function calcular(){
+function calcular() {
     const numero = Number(document.getElementById('texto').value);
-    
-    let raiz = document.getElementById('raiz')
-    let int = document.getElementById('int')
-    let nan = document.getElementById('nan')
-    let arrBaixo = document.getElementById('arrBaixo')
-    let arrCima = document.getElementById('arrCima')
-    let dec = document.getElementById('dec')
-    
-    raiz.innerHTML = `<p>Raiz quadrada: ${Math.hypot(numero)} </p>`
-    int.innerHTML = ` <p> ${numero} é inteiro? ${Math.numero}</p>`
-    nan.innerHTML = `<p>${numero} é válido? ${numero}</p>`
-    arrBaixo.innerHTML = `<p>Arrendondando para baixo: ${numero}</p>`
-    arrCima.innerHTML = `<p>Arrendondando para cima: ${numero}</p>`
-    dec.innerHTML = `<p>Com duas casas decimais: ${numero}</p>`
-    }
-    
+
+    let raiz = document.getElementsByClassName('raiz')
+    let int = document.getElementsByClassName('int')
+    let nan = document.getElementsByClassName('nan')
+    let arrBaixo = document.getElementsByClassName('arrBaixo')
+    let arrCima = document.getElementsByClassName('arrCima')
+    let dec = document.getElementsByClassName('dec')
+
+    raiz[0].innerHTML = `<p>Raiz quadrada: ${numero ** 0.5} </p>`
+    int[0].innerHTML = ` <p> ${numero} é inteiro? ${Number.isInteger(numero)} </p>`
+    nan.innerHTML = `<p>${numero} é válido? ${Number.isNaN(numero)}</p>`
+    arrBaixo[0].innerHTML = `<p>Arrendondando para baixo: ${Math.floor(numero)}</p>`
+    arrCima[0].innerHTML = `<p>Arrendondando para cima: ${Math.ceil(numero)}</p>`
+    dec[0].innerHTML = `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
+}
 
