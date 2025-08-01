1f7cab4cb1d6c4e876808c746c347b22e3727820
Sávio Sérgio <168147001+SavioSrg@users.noreply.github.com>
2025-08-01 18:00:16 -0300

    organização da estrutura de pastas

---
+++ courses/curso-one-T9/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,12 @@
+// Carregando a mensagem inicial para incio do jogo
+Alert('Boas vinda ao jogo do número secreto');
+
+//Criação de uma variavél
+let numeroSecreto = 5;
+
+//Campo de interação com o usúario
+prompt ('Escolha um número entre 1 e 10');
+
+
+//Condição
+// if (numeroSecreto == )
