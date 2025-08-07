db8792afe54f773203d158cb18124f81a384260b
Sávio Sérgio <168147001+SavioSrg@users.noreply.github.com>
2025-08-07 10:03:34 -0300

    modulo 1 - alura lógica de programação

---
+++ courses/curso-one-T9/logica-js-projeto_inicial/desafios/ds001/scripts/app.js
@@ -0,0 +1,18 @@
+alert("Boas vindas ao nosso site");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+let mensagemDeErro = "Erro! Preencha todos campos";
+alert(mensagemDeErro);
+
+let nome_novo = prompt("Digite o seu nome");
+let idade_nova = prompt("Digite a sua idade");
+
+let idade_verificacao = prompt("Digite a sua idade");
+if (idade >= 18) {
+    alert ("Pode tirar a habilitação!")
+}
