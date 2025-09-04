d68248e5bcf491678c8efca902d78c4b3a30381d
Sávio Sérgio <168147001+SavioSrg@users.noreply.github.com>
2025-09-04 17:02:09 -0300

    Implementa uma função para agregar amigos

--- courses/curso-one-T9/Challenge-amigo-secreto/app.js
+++ courses/curso-one-T9/Challenge-amigo-secreto/app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome =input.value.trim(); //trim() remove espaços extras
+
+    // Validação
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+    }
+
+    amigos.push(nome); // Adiciona ao array
+    atualizarLista();  // Atualiza a lista
+    input.value = "";  // Limpa o campo
+}
